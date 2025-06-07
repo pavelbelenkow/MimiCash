02c1fab1721757cdd2ac4453432ff99149c4b766
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2025-06-07 19:06:24 +0300

    Initial Commit

---
+++ MimiCash/MimiCashApp.swift
@@ -0,0 +1,17 @@
+//
+//  MimiCashApp.swift
+//  MimiCash
+//
+//  Created by Pavel Belenkow on 07.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct MimiCashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
