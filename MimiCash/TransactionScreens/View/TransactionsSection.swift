a6562fbb7353884e4bd8b2631dea8a3636d7e551
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2025-06-21 06:42:42 +0300

    Added TransactionsSection to display transactions

---
+++ MimiCash/TransactionScreens/View/TransactionsSection.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct TransactionsSection<Header: View>: View {
+    let output: TransactionsOutput
+    let isHistory: Bool
+    @ViewBuilder let header: () -> Header
+    
+    var body: some View {
+        List {
+            Section {
+                header()
+                
+                HStack {
+                    Text(isHistory ? "Сумма" : "Всего")
+                    Spacer()
+                    Text(output.total.formatted() + " ₽")
+                }
+            }
+            
+            Section(header: Text("Операции").font(.subheadline)) {
+                if !output.transactions.isEmpty {
+                    ForEach(output.transactions, id: \.id) {
+                        TransactionRowView(transaction: $0, isHistoryView: isHistory)
+                            .frame(height: isHistory ? 60 : 44)
+                            .listRowInsets(.init(top: 0, leading: 16, bottom: 0, trailing: 16))
+                    }
+                } else {
+                    Text("Нет операций в заданном диапазоне")
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                }
+            }
+        }
+        .listSectionSpacing(.zero)
+        .safeAreaPadding(.top)
+    }
+}
