59c5db7b99d266ff214ef857cc037098427b90e6
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2025-06-22 14:19:05 +0300

    FIxed date

--- MimiCash/Extension/Date+Extensions.swift
+++ MimiCash/Extension/Date+Extensions.swift
@@ -8,14 +8,17 @@ extension Date {
     static var endOfToday: Date {
         let calendar = Calendar.current
         return calendar.date(
-            bySettingHour: 23,
-            minute: 59,
-            second: 59,
-            of: startOfToday
+            byAdding: .day,
+            value: 1,
+            to: startOfToday
         ) ?? Date()
     }
     
     static var monthAgo: Date {
-        Calendar.current.date(byAdding: .month, value: -1, to: Date()) ?? Date()
+        Calendar.current.date(
+            byAdding: .month,
+            value: -1,
+            to: Date()
+        ) ?? Date()
     }
 }
