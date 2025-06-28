ec52fda134fd201733de43631dc8668bf5b01969
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2025-06-28 04:55:06 +0300

    Added init and Spec enum to EntityView

--- MimiCash/TransactionScreens/View/EntityView.swift
+++ MimiCash/TransactionScreens/View/EntityView.swift
@@ -1,11 +1,28 @@
 import SwiftUI
 
 struct EntityView<Content, Output>: View where Content: View {
+    
+    // MARK: - Properties
     let state: ViewState<Output>
     let title: String
     let loadingText: String
     let errorPrefix: String
     let content: (Output) -> Content
+    
+    // MARK: - Init
+    init(
+        state: ViewState<Output>,
+        title: String,
+        loadingText: String = Spec.loadingText,
+        errorPrefix: String = Spec.errorPrefix,
+        content: @escaping (Output) -> Content
+    ) {
+        self.state = state
+        self.title = title
+        self.loadingText = loadingText
+        self.errorPrefix = errorPrefix
+        self.content = content
+    }
     
     var body: some View {
         VStack {
@@ -24,4 +41,11 @@ struct EntityView<Content, Output>: View where Content: View {
         }
         .navigationTitle(title)
     }
+}
+
+// MARK: - Spec
+
+private enum Spec {
+    static let loadingText = "Загружаемся..."
+    static let errorPrefix = "Ошибка: "
 }
