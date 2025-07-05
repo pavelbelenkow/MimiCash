6eff851b4825a0cf3b5c7fd8eb8ea4bc47749557
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2025-07-05 04:06:53 +0300

    Implemented CategoriesView

---
+++ MimiCash/CategoriesScreen/View/CategoriesView.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct CategoriesView: View {
+    
+    @State private var viewModel: CategoriesViewModel
+    
+    // MARK: - Init
+    init(viewModel: CategoriesViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        NavigationStack {
+            EntityView(
+                state: viewModel.viewState,
+                title: Spec.title,
+                content: CategoriesListView.init
+            )
+        }
+        .tint(.navBar)
+        .searchable(text: $viewModel.searchText)
+        .task { await viewModel.loadCategories() }
+    }
+}
+
+// MARK: - Spec
+
+private enum Spec {
+    static let title = "Мои статьи"
+}
+
+// MARK: - Preview
+
+#Preview {
+    CategoriesView(
+        viewModel: CategoriesViewModelImp()
+    )
+}
