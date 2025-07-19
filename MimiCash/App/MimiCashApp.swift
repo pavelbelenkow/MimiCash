e5e890f449935ed615ee15628e828ff65198130b
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2025-07-19 05:59:42 +0300

    Added setting up bearer token to storage

--- MimiCash/App/MimiCashApp.swift
+++ MimiCash/App/MimiCashApp.swift
@@ -2,9 +2,18 @@ import SwiftUI
 
 @main
 struct MimiCashApp: App {
+    
+    init() {
+        setupBearerToken("YOUR_BEARER_TOKEN_HERE")
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+    
+    private func setupBearerToken(_ token: String) {
+        KeychainTokenStorage.shared.setToken(token)
     }
 }
