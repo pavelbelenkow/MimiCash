9f3fe25569c7b9f53edd135f5ea9f6dfd6aadbcb
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2025-07-23 01:44:02 +0300

    Refactored service coordinator

--- MimiCash/Service/ServiceCoordinator.swift
+++ MimiCash/Service/ServiceCoordinator.swift
@@ -4,83 +4,37 @@ import Foundation
 
 protocol ServiceCoordinator {
     func transactionCreated(_ transaction: Transaction) async
-    func transactionUpdated(_ transaction: Transaction) async
-    func transactionDeleted(transactionId: Int, accountId: Int) async
+    func transactionUpdated(old: Transaction, new: Transaction) async
+    func transactionDeleted(_ transaction: Transaction) async
 }
 
 final class ServiceCoordinatorImp: ServiceCoordinator {
     
     // MARK: - Private Properties
     private let bankAccountsService: BankAccountsService
-    private let backupStorage: BackupStorage
     
     // MARK: - Init
-    init(
-        bankAccountsService: BankAccountsService,
-        backupStorage: BackupStorage
-    ) {
+    init(bankAccountsService: BankAccountsService) {
         self.bankAccountsService = bankAccountsService
-        self.backupStorage = backupStorage
     }
     
     // MARK: - ServiceCoordinator Implementation
     
     func transactionCreated(_ transaction: Transaction) async {
-        await addAccountUpdateToBackup(account: transaction.account)
+        _ = try? await bankAccountsService.applyLocalTransactionCreated(transaction)
     }
     
-    func transactionUpdated(_ transaction: Transaction) async {
-        await addAccountUpdateToBackup(account: transaction.account)
-    }
-    
-    func transactionDeleted(transactionId: Int, accountId: Int) async {
-        do {
-            let account = try await bankAccountsService.fetchCurrentAccount()
-            if account.id == accountId {
-                await addAccountUpdateToBackup(account: account)
-            }
-        } catch {
-            let fallbackAccount = BankAccount(
-                id: accountId,
-                name: "Account",
-                balance: 0,
-                currency: "RUB"
-            )
-            await addAccountUpdateToBackup(account: fallbackAccount)
-        }
-    }
-    
-    // MARK: - Private Methods
-    
-    private func addAccountUpdateToBackup(account: BankAccount) async {
-        let accountData = account.toAccountUpdateBody()
-        let backupOperation = BackupOperation(
-            entityId: account.id,
-            entityType: .account,
-            operationType: .update,
-            transactionData: nil,
-            accountData: accountData
+    func transactionUpdated(
+        old oldTransaction: Transaction,
+        new newTransaction: Transaction
+    ) async {
+        _ = try? await bankAccountsService.applyLocalTransactionUpdated(
+            old: oldTransaction,
+            new: newTransaction
         )
-        
-        let existingOperations = await backupStorage.getBackupOperations(
-            entityId: account.id,
-            entityType: .account
-        )
-        
-        if existingOperations.isEmpty {
-            await backupStorage.addBackupOperation(backupOperation)
-        } else {
-            if let _ = existingOperations.first {
-                let updatedOperation = BackupOperation(
-                    entityId: account.id,
-                    entityType: .account,
-                    operationType: .update,
-                    transactionData: nil,
-                    accountData: accountData
-                )
-                await backupStorage.update(updatedOperation)
-            }
-            
-        }
+    }
+    
+    func transactionDeleted(_ transaction: Transaction) async {
+        _ = try? await bankAccountsService.applyLocalTransactionDeleted(transaction)
     }
 }
